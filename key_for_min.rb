b5f230b5679fda00088f3f745d18e4535826fb7a
Ursula Miller <ursula.miller@gmail.com>
2019-06-17 18:47:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  array_of_values=[]
+  lowest_value = 10000
+  name_hash.each do |key, value|
+  array_of_values << value
+  end  
+  if
+      
+  else  
+  
+  end
 end
