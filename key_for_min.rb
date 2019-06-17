91930e9d4f900f01a8a2e6061fefe3bce67b949a
Ursula Miller <ursula.miller@gmail.com>
2019-06-17 19:19:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-name_hash = {:chair => 25, :table => 85, :mattress => 450}
+ikea = {:chair => 25, :table => 85, :mattress => 450}
 
 
 
@@ -28,4 +28,4 @@ def key_for_min_value(name_hash)
   end
 end
 
-key_for_min_value(name_hash)
+key_for_min_value(ikea)
