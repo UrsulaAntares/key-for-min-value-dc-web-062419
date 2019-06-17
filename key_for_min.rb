615a8f776d132f4565ec1fec1280438e3592f56e
Ursula Miller <ursula.miller@gmail.com>
2019-06-17 18:55:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,10 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+name_hash = {}
+
+
+
 def key_for_min_value(name_hash)
   array_of_values=[]
   lowest_value = 10000
