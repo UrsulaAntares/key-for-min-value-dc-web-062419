243db042e14bd97bea822d12f63cc4249598f0aa
Ursula Miller <ursula.miller@gmail.com>
2019-06-17 19:36:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -29,12 +29,8 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value == lowest_value
       return key
-    else
-      return nil
     end
-    
   end
-
 end
 
 key_for_min_value(ikea)
