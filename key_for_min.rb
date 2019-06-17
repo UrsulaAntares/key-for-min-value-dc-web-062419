2aa15dfcb31e1cd64a20eb376f4fd16d1c5e55e7
Ursula Miller <ursula.miller@gmail.com>
2019-06-17 19:26:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ ikea = {:table => 85, :mattress => 450, :chair => 25}
 def key_for_min_value(name_hash)
   array_of_values=[]
   lowest_value = 10000
-  i=0
+
   cheapest_thing = "TBD"
   name_hash.each do |key, value|
   array_of_values << value
@@ -16,13 +16,14 @@ def key_for_min_value(name_hash)
 #  puts value
 #  puts key
   while i<array_of_values.length    
+    i=0
     if value < lowest_value
         lowest_value = value
 #       cheapest_thing = key
-        puts "The lowest value is #{lowest_value}}"
+        puts "The new lowest value is #{lowest_value}}"
     end
     i+=1
-    return cheapest_thing
+#    return cheapest_thing
   end
   puts array_of_values
   print cheapest_thing
