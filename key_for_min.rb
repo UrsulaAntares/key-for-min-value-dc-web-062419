b661a1add0eed8c45818ea8c0bdd28404063d8bf
Ursula Miller <ursula.miller@gmail.com>
2019-06-17 19:20:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,14 +11,14 @@ def key_for_min_value(name_hash)
   i=0
   cheapest_thing = "TBD"
   name_hash.each_with_index do |key, value|
-#  array_of_values << value
+  array_of_values << value
   puts value[2]
   puts key[2]
   while i<name_hash.length    
-    if value[i] < lowest_value
-        lowest_value = value[i]
+    if value < lowest_value
+        lowest_value = value
 #       cheapest_thing = key
-        puts "#{lowest_value} for #{value[i]}"
+        puts "#{lowest_value} for #{value}"
     end
     i+=1
     return cheapest_thing
