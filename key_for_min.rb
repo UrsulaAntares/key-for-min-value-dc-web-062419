38598fcc2f6d147c286d09d455c5239635da2f7c
Ursula Miller <ursula.miller@gmail.com>
2019-06-17 19:04:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ name_hash = {:chair => 25, :table => 85, :mattress => 450}
 
 def key_for_min_value(name_hash)
   array_of_values=[]
-  lowest_value = nil
+  lowest_value = 10000
   i=0
   cheapest_thing = "TBD"
   name_hash.each do |key, value|
