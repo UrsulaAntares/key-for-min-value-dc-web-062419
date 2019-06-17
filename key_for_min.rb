41570766f949d887eee70738d270fce00924e643
Ursula Miller <ursula.miller@gmail.com>
2019-06-17 19:08:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,15 +10,17 @@ def key_for_min_value(name_hash)
   lowest_value = 10000
   i=0
   cheapest_thing = "TBD"
-  name_hash.each_with_index do |key, value|
+  name_hash.each_with_index do |(key, value), i|
 #  array_of_values << value
       
   if value < lowest_value
       lowest_value = value
       cheapest_thing = key
       puts "#{lowest_value} for #{cheapest_thing}"
+      i+=1
       return cheapest_thing
   end
+  
   print cheapest_thing
   return cheapest_thing
   end
